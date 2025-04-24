2fb3c2f7dcd9f7df70dd8ef029e0537c4726080c
Eduard <SagaDev75@gmail.com>
2025-04-24 18:04:11 +0300

    feat: MonoStorage

---
+++ SmithTown/Assets/Saga/Scripts/SystemInitialization/MonoStorage.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Saga.SystemInitialization;
+using UnityEngine;
+
+namespace Saga.SystemInitialization
+{
+    public class MonoStorage<TSelf, TAsset> : MonoSingleton<TSelf>
+        where TSelf : MonoStorage<TSelf, TAsset>
+        where TAsset : Object
+    {
+        public virtual string FolderName { get; }
+
+        private readonly Dictionary<string, TAsset> _innerStorage = new();
+        public static IReadOnlyDictionary<string, TAsset> Storage => Singleton._innerStorage;
+        protected override void OnAwake()
+        {
+            var presets = Resources.LoadAll<TAsset>(FolderName);
+
+            foreach (var preset in presets)
+            {
+                if(_innerStorage.TryAdd(preset.name, preset)) continue;
+                
+                Debug.LogWarning($"Failed to load asset: {preset.name}. Asset Name already exists!");
+            }
+        }
+    }
+}
+
